5bb4623634538395a6609217bf63f1213f65d30d
NJ-KIM11 <alwaysred1116@gmail.com>
2024-12-05 15:10:13 +0900

    sdafd

---
+++ src/main/java/com/ohgiraffers/jenkins_test_app/config/CorsConfig.java
@@ -0,0 +1,17 @@
+package com.ohgiraffers.jenkins_test_app.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")  // 모든 요청에 대해서
+                .allowedOrigins("https://cloud-test-app-kappa.vercel.app/")    // 허용할 주소
+                .allowedMethods("GET", "POST", "PUT", "DELETE")     // 허용할 HTTP 메소드
+                .allowedHeaders("*");   // 허용할 헤더
+    }
+}
