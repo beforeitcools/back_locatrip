3a64eaa5f9342eab36c2b13ce105486eac206577
NJ-KIM11 <alwaysred1116@gmail.com>
2024-12-23 17:47:54 +0900

    jenkens test2

--- src/main/java/com/ohgiraffers/jenkins_test_app/user/controller/UserController.java
+++ src/main/java/com/ohgiraffers/jenkins_test_app/user/controller/UserController.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/menu")
-public class MenuController {
+public class UserController {
 
     @Autowired
     private MenuService menuService;
