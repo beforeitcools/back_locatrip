9f4a2dfae18f6cc7e56995886fa50f65fe62c42f
yzzzang <yz1211ch@gmail.com>
2025-01-17 16:34:55 +0900

    feat: post view page

--- src/main/java/com/ohgiraffers/jenkins_test_app/advice/controller/PostController.java
+++ src/main/java/com/ohgiraffers/jenkins_test_app/advice/controller/PostController.java
@@ -3,10 +3,8 @@ package com.ohgiraffers.jenkins_test_app.advice.controller;
 import com.ohgiraffers.jenkins_test_app.advice.entity.Posts;
 import com.ohgiraffers.jenkins_test_app.advice.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/post")
@@ -16,8 +14,15 @@ public class PostController
     private PostService postService;
 
     @PostMapping("/new")
-    public void insertNewPost(@RequestBody Posts post)
+    public ResponseEntity<Integer> insertNewPost(@RequestBody Posts post)
     {
-        postService.savePost(post);
+        return ResponseEntity.ok(postService.savePost(post));
+    }
+
+    @GetMapping("/select/{postId}")
+    public ResponseEntity<Posts> getPostById(@PathVariable int postId)
+    {
+        System.out.println("GETPOSTBYID로 접근은 햇냐고");
+        return postService.getPostById(postId);
     }
 }
--- src/main/java/com/ohgiraffers/jenkins_test_app/advice/service/PostService.java
+++ src/main/java/com/ohgiraffers/jenkins_test_app/advice/service/PostService.java
@@ -4,7 +4,10 @@ import com.ohgiraffers.jenkins_test_app.advice.entity.Posts;
 import com.ohgiraffers.jenkins_test_app.advice.repository.PostRepository;
 import com.ohgiraffers.jenkins_test_app.common.utils.SecurityUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class PostService
@@ -15,9 +18,17 @@ public class PostService
     @Autowired
     private SecurityUtil securityUtil;
 
-    public void savePost(Posts post)
+    public int savePost(Posts post)
     {
         post.setUserId(securityUtil.getAuthenticatedUser().getId());
-        postRepository.save(post);
+        return postRepository.save(post).getId();
+    }
+
+    public ResponseEntity<Posts> getPostById(int postId)
+    {
+        System.out.println("아 존나 잠온다 get Post by Id");
+        Optional<Posts> result = postRepository.findById(postId);
+        System.out.println("result: " + result);
+        return ResponseEntity.ok(result.get());
     }
 }
