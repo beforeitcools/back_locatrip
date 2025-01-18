70a94d21d260407ad45e017aca0d66825edeb3e1
jeongmin <mymemose@naver.com>
2025-01-18 16:53:44 +0900

    feat:메인수정

--- src/main/java/com/ohgiraffers/jenkins_test_app/main/controller/MainController.java
+++ src/main/java/com/ohgiraffers/jenkins_test_app/main/controller/MainController.java
@@ -1,13 +1,18 @@
 package com.ohgiraffers.jenkins_test_app.main.controller;
 
 import com.ohgiraffers.jenkins_test_app.auth.entity.Users;
+import com.ohgiraffers.jenkins_test_app.common.utils.SecurityUtil;
 import com.ohgiraffers.jenkins_test_app.main.service.MainService;
+import com.ohgiraffers.jenkins_test_app.mypage.service.MypageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("main")
@@ -16,6 +21,12 @@ public class MainController {
     @Autowired
     MainService mainService;
 
+    @Autowired
+    MypageService mypageService;
+
+    @Autowired
+    SecurityUtil securityUtil;
+
     @GetMapping("getUserInfo/{hostId}")
     public ResponseEntity getUserInfo(@PathVariable Integer hostId) {
 
@@ -23,16 +34,21 @@ public class MainController {
             return ResponseEntity.badRequest().build();
         }
 
-
         Users users = mainService.getUserInfo(hostId);
-
 
         if(users == null){
             return ResponseEntity.noContent().build();
         }
 
+        // 프로필 info
+        Users authenticatedUser = securityUtil.getAuthenticatedUser();
+        // 알림(안 읽은거 있는지)
+        boolean unreadAlarmExists = mypageService.getUnreadAlarmExists(authenticatedUser.getId());
 
+        Map<String, Object> result = new HashMap<>();
+        result.put("users", users);
+        result.put("unreadAlarmExists", unreadAlarmExists);
 
-        return ResponseEntity.ok(users);
+        return ResponseEntity.ok(result);
     }
 }
