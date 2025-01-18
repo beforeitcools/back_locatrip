778ab23c92287cb4e7eb1a56ad4cea55641ac601
NJ-KIM11 <alwaysred1116@gmail.com>
2025-01-19 00:26:52 +0900

    feat: 내여행 selectedRegion count 에서 list 로 바꿈

--- src/main/java/com/ohgiraffers/jenkins_test_app/common/ServerUrlConstants.java
+++ src/main/java/com/ohgiraffers/jenkins_test_app/common/ServerUrlConstants.java
@@ -2,6 +2,7 @@ package com.ohgiraffers.jenkins_test_app.common;
 
 public class ServerUrlConstants {
 
-    public static final String BACK_URL = "https://www.beforeitcools.site:7777";
+    public static final String BACK_URL = "http://112.221.66.174:1102";
 
+//    public static final String BACK_URL = "https://www.beforeitcools.site:7777";
 }
