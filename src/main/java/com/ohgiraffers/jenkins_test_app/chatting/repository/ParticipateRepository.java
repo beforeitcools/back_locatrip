215409b9a496ac43178fa226fbb51ed8b308e147
yzzzang <yz1211ch@gmail.com>
2025-01-20 10:08:32 +0900

    feat: 채팅룸 찾기 및 로직 일부 수정

--- src/main/java/com/ohgiraffers/jenkins_test_app/chatting/repository/ParticipateRepository.java
+++ src/main/java/com/ohgiraffers/jenkins_test_app/chatting/repository/ParticipateRepository.java
@@ -2,10 +2,15 @@ package com.ohgiraffers.jenkins_test_app.chatting.repository;
 
 import com.ohgiraffers.jenkins_test_app.chatting.entity.ParticipateMembers;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface ParticipateRepository extends JpaRepository<ParticipateMembers, Integer>
 {
 
+    @Query("SELECT COUNT(*) FROM ParticipateMembers p WHERE p.chatroom.id = :chatroomId AND p.userId = :userId")
+    Optional<Integer> findUserExists(Integer chatroomId, Integer userId);
 }
